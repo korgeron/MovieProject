faec5522e13e30cdc33662d413f321e79806ca90
kevin <orgeron.kevin@gmail.com>
2023-04-07 20:32:25 -0500

    form set up for api call

---
+++ index.js
@@ -0,0 +1,21 @@
+let API_KEY;
+let MOVIE_NAME;
+
+const submitAPI_KEY = document.querySelector('.submit-key');
+const apiKeyForm = document.querySelector('.todo-list');
+const bodyElement = document.querySelector('body');
+
+submitAPI_KEY.addEventListener('click', ()=>{
+    API_KEY = document.querySelector('.api-key').value;
+    MOVIE_NAME = document.querySelector('.movie-title').value;
+    apiKeyForm.style.display = 'none';
+
+
+
+
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${MOVIE_NAME}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    });
+})
