6c2f861adf54d41c452774f4f2d80258b54438c1
kevin <orgeron.kevin@gmail.com>
2023-04-07 21:48:20 -0500

    trying to fix mixed content error

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let API_KEY;
 let MOVIE_NAME;
-let URL = 'https://www/omdbapi.com/?apikey=' + API_KEY + '&s=' + MOVIE_NAME;
 
 const submitAPI_KEY = document.querySelector('.submit-key');
 const apiKeyForm = document.querySelector('.todo-list');
@@ -9,6 +8,7 @@ const bodyElement = document.querySelector('body');
 submitAPI_KEY.addEventListener('click', ()=>{
     API_KEY = document.querySelector('.api-key').value;
     MOVIE_NAME = document.querySelector('.movie-title').value;
+    const URL = 'https://www/omdbapi.com/?apikey=' + API_KEY + '&s=' + MOVIE_NAME;
     apiKeyForm.style.display = 'none';
 
 
