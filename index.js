21e00747e7e95654854de5ce761cb3d613020491
kevin <orgeron.kevin@gmail.com>
2023-04-09 22:47:04 -0500

    working on mobile / desktop

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const movieSection = document.querySelector('.movie-section');
 
 submitAPI_KEY.addEventListener('click', ()=>{
     API_KEY = document.querySelector('.api-key').value;
-    MOVIE_NAME = document.querySelector('.movie-title').value;
+    MOVIE_NAME = document.querySelector('#movie-title').value;
     let URL = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${MOVIE_NAME}`
     apiKeyForm.style.display = 'none';
     movieSection.style.display = 'grid';
