19db0b40f0303412e35549614e00ebfa33a292e8
kevin <orgeron.kevin@gmail.com>
2023-04-10 20:29:01 -0500

    may be finished

--- index.js
+++ index.js
@@ -7,13 +7,38 @@ const apiKeyForm = document.querySelector('.todo-list');
 const bodyElement = document.querySelector('body');
 const movieSection = document.querySelector('.movie-section');
 const movieImageBox = document.querySelector('.movie-image');
+const movieInfo = document.querySelector('.movie-info');
+const resetButton = document.querySelector('.reset');
+
+resetButton.addEventListener('click', ()=>{
+    window.location.reload();
+})
 
 const populateMovieData = (movieID , key) => {
     fetch(`https://www.omdbapi.com/?apikey=${key}&i=${movieID}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.Poster);
+            movieInfo.innerHTML = "";
+
+            console.log(data);
             movieImageBox.style.backgroundImage = `url(${data.Poster})`;
+            movieInfo.innerHTML +=
+            `
+                <div style="text-align: center">
+                    <h1>${data.Title}</h1>
+                </div>
+                <div style="display: flex; justify-content: space-between">
+                    <p style="padding: 0 1vw 0 1vw">${data.Type.toUpperCase()} : ${data.Rated}</p>
+                    <p style="padding: 0 1vw 0 1vw">${data.Country}</p>
+                    <p style="padding: 0 1vw 0 1vw">${data.Language}</p>
+                </div>
+                <div style="text-align: center">
+                    <p>${data.Plot}</p>
+                </div>
+                <div style="text-align: center">
+                    <p>Actors: ${data.Actors}</p>
+                </div>
+            `
         })
 }
 
@@ -33,15 +58,26 @@ submitAPI_KEY.addEventListener('click', ()=>{
         data.Search.forEach(movie => {
             movieContainer.innerHTML +=
                 `<div class="movie-card" style="background-image: url(${movie.Poster})">
-                    <div class="movie-title">${movie.Title}</div>
                     <input class="movie-id" type="hidden" value="${movie.imdbID}" />
+                    <div class="movie-title">${movie.Title}</div>
                 </div>`;
         })
 
-       let movieIDs = document.querySelectorAll('.movie-id');
-        movieIDs.forEach((id , i) => {
-            if (i == 0) populateMovieData(id.value, API_KEY);
-        })
+            let movieIDs = document.querySelectorAll('.movie-id');
+            let movies = document.querySelectorAll('.movie-card');
+
+            movieIDs.forEach((id , i) => {
+                if (i == 0) populateMovieData(id.value, API_KEY);
+            })
+
+            movies.forEach(movieClick => {
+                movieClick.addEventListener('click', ()=>{
+                    let ID = movieClick.firstElementChild.value;
+                    populateMovieData(ID , API_KEY);
+                })
+
+            })
+
     })
 });
 
